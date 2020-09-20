91a3dad6b6a23e848f0cbcd628d6a3c62e84c764
Aniss-99 <anissghimire@gmail.com>
2020-09-20 12:15:10 +0545

    Calculated area of shapes using Inheritance concept

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -4,7 +4,19 @@ public class Program
 {
     static void Main()
     {
-        Collections collections = new Collections();
-        collections.LearnDictionary();
+        // Collections collections = new Collections();
+        // collections.LearnDictionary();
+        var square= new Square(20);
+        square.Display();   //var squareArea=square.GetArea();
+
+        // var rectangle =new Rectangle(20,30);
+        // var rectangleArea=rectangle.GetArea();
+
+        // var circle= new Circle(20);
+        // var circleArea= circle.GetArea();
+
+
+
+      
     }
 }
