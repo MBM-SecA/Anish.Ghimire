72a1969a1935e679d3ffeea2011133080ee6e939
Aniss-99 <anissghimire@gmail.com>
2020-09-28 19:34:52 +0545

    Learned about Asynchronus programming

--- Async/Program.cs
+++ Async/Program.cs
@@ -1,43 +1,45 @@
 ﻿using System;
-using System.Net.Https;
+using System.Net.Http;
 using System.Diagnostics;
-using System.Threading.
+using System.Threading.Tasks;
 
 namespace Async
 {
     class Program
     {
-        static async Main()
+        static async Task Main()
         {
-         //Making Breakfast
-           //Make Tea
-           //Turn on Burner(2s)
-           //Place kettle(2s)
-           //Pour the water(5s)
-           //Grind Masalas(60s)
-           //Put sugar and masalas(5s)
-           //Put Milk(3s)
-           //Let it Boil(5s)
+            // Making breakfast
 
-           ////Turn on Burner(2s)
-           //put pan and add butter(2s)
-           //Crack eggs and whisk it(2s)
-           //fry eggs(2s)
+            // Make Tea
+            // - Turn on burner (2s)
+            // - Place kettle on burner (3s)
+            // - Pour Water (5s)
+            // - Let it boil (5min)
+            // - Grind Masalas (1min)
+            // - Put Sugar and masalas (10s)
+            // - Pour Milk (5s)
+            // - Let it boil (3min)
 
-           Stopwatch stopwatch= Stopwatch.StartNew();
-           HttpClient client = new HttpClient();
-           var google=client.GetStringAsync("www.google.com").Result;
-           var m=client.GetStringAsync("www.microsoft.com").Result;
-           var y=client.GetStringAsync("www.yahoo.com").Result;
-           var g=client.GetStringAsync("https://github.com/MBM-SecA").Result;
-           Console.WriteLine($"{stopwatch.ElapsedMilliseconds}ms");
+            // Fry egg
+            // - Turn on burner (2s)
+            // - Put pan and oil it (30s)
+            // - Crack eggs and whisk it (3min)
+            // - Fry (3min)
+            HttpClient client = new HttpClient();
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            var google = client.GetStringAsync("www.google.com").Result;
+            var microsoft = client.GetStringAsync("www.microsoft.com").Result;
+            var yahoo = client.GetStringAsync("www.yahoo.com").Result;
+            var git = client.GetStringAsync("www.github.com/MBM-SecA/Sushan.ghimire").Result;
+            Console.WriteLine($"It took {stopwatch.ElapsedMilliseconds} ms");
 
-            var google=await client.GetStringAsync("www.google.com");
-           var m=await client.GetStringAsync("www.microsoft.com");
-           var y=await client.GetStringAsync("www.yahoo.com");
-           var g=await client.GetStringAsync("github.com/MBM-SecA");
-           Console.WriteLine($"{stopwatch.ElapsedMilliseconds}ms");
-
+            stopwatch.Restart();
+            var g = await client.GetStringAsync("www.google.com");
+            var m = await client.GetStringAsync("www.microsoft.com");
+            var y = await client.GetStringAsync("www.yahoo.com");
+            var gi =await client.GetStringAsync("www.github.com/MBM-SecA/Sushan.ghimire");
+            Console.WriteLine($"It took {stopwatch.ElapsedMilliseconds} ms");
 
         }
     }
