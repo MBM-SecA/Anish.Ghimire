ac3dd49a269ab54c2aa59764c82fe99e30f60000
Aniss-99 <anissghimire@gmail.com>
2020-12-03 09:58:12 +0545

    Setting Up The Database

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -1,48 +1,35 @@
-using System.Collections.Generic;
+using System;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using System;
 
-
-public class EmployeeController:Controller
+public class EmployeeController : Controller
 {
     public ActionResult Index()
     {
-        
-        List<Person> employees= Person.GetPerson();
-        
-        
+        var employees = Person.GetPerson();
+       
         return View(employees);
-
     }
-    public ActionResult Details([FromQuery] int id)
+
+    public ActionResult Detail([FromQuery]int id)
     {
-    //    List<Person> employees = Person.GetPerson();
-    //     foreach(var employee in employees)
-    //     {
-    //         if(employee.FirstName == firstName)
-    //         {
-    //             return View(employee);
-    //         }
-    //     }
-    //     return View();
-    var employees= Person.GetPerson();
-    var employee=employees.FirstOrDefault(x=>x.Id==id);
-    return View(employee);
+        var employees = Person.GetPerson();
+        var employee = employees.FirstOrDefault(x => x.Id == id);
 
-
+        return View(employee);
     }
 
     public ActionResult Add()
     {
         return View();
-
     }
+
     [HttpPost]
-    public ActionResult<string> Add(Person person)
+    public ActionResult<string> Add([FromForm] Person person)
     {
-        return "Record saved";
-
+        return "Record Saved";
+        
     }
+}
 
 }
